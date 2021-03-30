15b1fc3691d2c31bdfd46cbe4937d36210cd8c97
Socket1 <70174021+Socket1@users.noreply.github.com>
2021-03-30 13:47:44 -0400

    Initialize npai/daily-reporter-end-date

---
+++ liquidator/index.js
@@ -0,0 +1,167 @@
+require("dotenv").config();
+
+// Helpers
+const { delay } = require("../financial-templates-lib/helpers/delay");
+const { startServer } = require("../common/ServerUtils");
+const { Logger, waitForLogger } = require("../financial-templates-lib/logger/Logger");
+const { MAX_UINT_VAL } = require("../common/Constants");
+const { toBN } = web3.utils;
+
+// JS libs
+const { Liquidator } = require("./liquidator");
+const { GasEstimator } = require("../financial-templates-lib/helpers/GasEstimator");
+const { ExpiringMultiPartyClient } = require("../financial-templates-lib/clients/ExpiringMultiPartyClient");
+const { createPriceFeed } = require("../financial-templates-lib/price-feed/CreatePriceFeed");
+const { Networker } = require("../financial-templates-lib/price-feed/Networker");
+
+// Truffle contracts
+const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
+const ExpandedERC20 = artifacts.require("ExpandedERC20");
+
+// TODO: Figure out a good way to run this script, maybe with a wrapper shell script.
+// Currently, you can run it with `truffle exec ../liquidator/index.js --address=<address> --price=<price>` *from the core
+// directory*.
+
+/**
+ * @notice Continuously attempts to liquidate positions in the EMP contract.å
+ * @param {Number} price Price used to determine undercollateralized positions to liquidate.
+ * @param {String} address Contract address of the EMP.
+ * @param {Boolean} shouldPoll If False, then exit after one iteration. Used for testing.
+ * @param {Number} pollingDelay The amount of milliseconds to wait between iterations.
+ * @param {Number} [monitorPort] Monitor server port number.
+ * @return None or throws an Error.
+ */
+async function run(address, shouldPoll, pollingDelay, priceFeedConfig, monitorPort, liquidatorConfig) {
+  try {
+    Logger.info({
+      at: "Liquidator#index",
+      message: "Liquidator started 🌊",
+      empAddress: address,
+      pollingDelay: pollingDelay,
+      priceFeedConfig,
+      liquidatorConfig,
+      monitorPort
+    });
+
+    // Setup web3 accounts an contract instance
+    const accounts = await web3.eth.getAccounts();
+    const emp = await ExpiringMultiParty.at(address);
+
+    // Setup price feed.
+    // TODO: consider making getTime async and using contract time.
+    const getTime = () => Math.round(new Date().getTime() / 1000);
+    const priceFeed = await createPriceFeed(Logger, web3, new Networker(Logger), getTime, priceFeedConfig);
+
+    if (!priceFeed) {
+      throw "Price feed config is invalid";
+    }
+
+    // Client and liquidator bot
+    const empClient = new ExpiringMultiPartyClient(Logger, ExpiringMultiParty.abi, web3, emp.address);
+    const gasEstimator = new GasEstimator(Logger);
+    const liquidator = new Liquidator(Logger, empClient, gasEstimator, priceFeed, accounts[0], liquidatorConfig);
+
+    // The EMP requires approval to transfer the liquidator's collateral and synthetic tokens in order to liquidate
+    // a position. We'll set this once to the max value and top up whenever the bot's allowance drops below
+    // MAX_INT / 2.
+    const collateralToken = await ExpandedERC20.at(await emp.collateralCurrency());
+    const syntheticToken = await ExpandedERC20.at(await emp.tokenCurrency());
+    const currentCollateralAllowance = await collateralToken.allowance(accounts[0], empClient.empAddress);
+    const currentSyntheticAllowance = await syntheticToken.allowance(accounts[0], empClient.empAddress);
+    if (toBN(currentCollateralAllowance).lt(toBN(MAX_UINT_VAL).div(toBN("2")))) {
+      const collateralApprovalTx = await collateralToken.approve(empClient.empAddress, MAX_UINT_VAL, {
+        from: accounts[0]
+      });
+      Logger.info({
+        at: "Liquidator#index",
+        message: "Approved EMP to transfer unlimited collateral tokens 💰",
+        collateralApprovalTx: collateralApprovalTx.transactionHash
+      });
+    }
+    if (toBN(currentSyntheticAllowance).lt(toBN(MAX_UINT_VAL).div(toBN("2")))) {
+      const syntheticApprovalTx = await syntheticToken.approve(empClient.empAddress, MAX_UINT_VAL, {
+        from: accounts[0]
+      });
+      Logger.info({
+        at: "Liquidator#index",
+        message: "Approved EMP to transfer unlimited synthetic tokens 💰",
+        collateralApprovalTx: syntheticApprovalTx.transactionHash
+      });
+    }
+
+    // Start monitoring server, which should listen for incoming requests as long as this bot is alive.
+    const { server, portNumber } = startServer(monitorPort);
+    Logger.debug({
+      at: "Liquidator#index",
+      message: "Monitor server is listening",
+      portNumber: portNumber
+    });
+
+    while (true) {
+      // Steps:
+      // Get most recent price from a price feed.
+      // Call client.getUnderCollateralizedPositions()
+      // Acquire synthetic tokens somehow. v0: assume the bot holds on to them.
+      // Liquidate any undercollateralized positions!
+      // Withdraw money from any liquidations that are expired or DisputeFailed.
+      await liquidator.queryAndLiquidate();
+      await liquidator.queryAndWithdrawRewards();
+
+      await delay(Number(pollingDelay));
+
+      if (!shouldPoll) {
+        break;
+      }
+    }
+
+    // TODO: I'm not sure if this actually is working as expected.
+    // Close server gracefully.
+    server.close();
+  } catch (error) {
+    console.log(error);
+    Logger.error({
+      at: "Liquidator#index",
+      message: "Liquidator polling error🚨",
+      error: new Error(error)
+    });
+    await waitForLogger(Logger);
+  }
+}
+
+const Poll = async function(callback) {
+  try {
+    if (!process.env.EMP_ADDRESS) {
+      throw "Bad input arg! Specify an `EMP_ADDRESS` for the location of the expiring Multi Party within your environment variables.";
+    }
+
+    const pollingDelay = process.env.POLLING_DELAY ? process.env.POLLING_DELAY : 10000;
+
+    if (!process.env.PRICE_FEED_CONFIG) {
+      throw "Bad input arg! Specify an `PRICE_FEED_CONFIG` for the location of the expiring Multi Party within your environment variables.";
+    }
+
+    const priceFeedConfig = JSON.parse(process.env.PRICE_FEED_CONFIG);
+
+    // If there is a liquidator config, add it. Else, set to null.
+    const liquidatorConfig = process.env.LIQUIDATOR_CONFIG ? process.env.LIQUIDATOR_CONFIG : null;
+
+    const portNumber = 8888;
+
+    await run(process.env.EMP_ADDRESS, true, pollingDelay, priceFeedConfig, portNumber, liquidatorConfig);
+  } catch (error) {
+    Logger.error({
+      at: "Liquidator#index",
+      message: "Liquidator configuration error🚨",
+      error: new Error(error)
+    });
+    await waitForLogger(Logger);
+    callback(error);
+    return;
+  }
+  callback();
+};
+
+// Attach this function to the exported function
+// in order to allow the script to be executed through both truffle and a test runner.
+Poll.run = run;
+module.exports = Poll;
---
+++ monitors/CRMonitor.js
@@ -0,0 +1,163 @@
+const { createFormatFunction, createEtherscanLinkMarkdown } = require("../common/FormattingUtils");
+
+// This module is used to monitor a list of addresses and their associated Collateralization ratio.
+class CRMonitor {
+  /**
+   * @notice Constructs new Collateral Requirement Monitor.
+   * @param {Object} logger an instance of a winston logger used to emit messages, logs and errors.
+   * @param {Object} expiringMultiPartyClient an instance of the expiringMultiPartyClient from the `financial-templates
+   *                 lib which provides synchronous access to positions opened against an expiring multiparty
+   * @param {Object} walletsToMonitor array of wallets to Monitor. Each wallet's `walletName`, `address`, `crAlert`
+   *                 must be given:
+   *                 [{ name: "Market Making bot",
+   *                    address: "0x12345",
+   *                    crAlert: 1.50 }, // Note 150% is represented as 1.5
+   *                  ...];
+   * @param {Object} priceFeed offchain price feed used to track the token price.
+   */
+  constructor(logger, expiringMultiPartyClient, walletsToMonitor, priceFeed) {
+    this.logger = logger;
+
+    this.empClient = expiringMultiPartyClient;
+    this.web3 = this.empClient.web3;
+
+    // Offchain price feed to compute the current collateralization ratio for the monitored positions.
+    this.priceFeed = priceFeed;
+
+    // Wallet addresses and thresholds to monitor.
+    this.walletsToMonitor = walletsToMonitor;
+
+    this.formatDecimalString = createFormatFunction(this.web3, 2, 4);
+
+    // TODO: replace this with a fetcher that pulls the actual collateral token symbol
+    this.collateralCurrencySymbol = "DAI";
+    this.syntheticCurrencySymbol = "ETHBTC";
+
+    // TODO: pull this into the parent client
+    this.networkId = 1;
+  }
+
+  // Queries all monitored wallet ballance for collateralization ratio against a given threshold.
+  async checkWalletCrRatio() {
+    // yield the price feed at the current time.
+    const price = this.priceFeed.getCurrentPrice();
+
+    if (!price) {
+      this.logger.warn({
+        at: "CRMonitor",
+        message: "Cannot compute wallet collateralization ratio because price feed returned invalid value"
+      });
+      return;
+    }
+
+    this.logger.debug({
+      at: "CRMonitor",
+      message: "Checking wallet collateralization ratios",
+      price: price.toString()
+    });
+    // For each monitored wallet check if the current collaterlization ratio is below the monitored threshold.
+    // If it is, then send an alert of formatted markdown text.
+    for (let wallet of this.walletsToMonitor) {
+      const positionInformation = this._getPositionInformation(wallet.address);
+      if (positionInformation == null) {
+        // There is no position information for the given wallet. Next run this will be updated as it is now enqueued.
+        continue;
+      }
+
+      const collateral = positionInformation.amountCollateral;
+      const tokensOutstanding = positionInformation.numTokens;
+
+      // If the values for collateral or price have yet to resolve, dont push a notification.
+      if (collateral == null || tokensOutstanding == null) {
+        continue;
+      }
+
+      // If CR = null then there are no tokens outstanding and so dont push a notification.
+      const positionCR = this._calculatePositionCRPercent(collateral, tokensOutstanding, price);
+      if (positionCR == null) {
+        continue;
+      }
+
+      // Lastly, if we have gotten a position CR ratio this can be compared against the threshold. If it is below the
+      // threshold then push the notification.
+      if (this._ltThreshold(positionCR, this.web3.utils.toWei(wallet.crAlert.toString()))) {
+        const liquidationPrice = this._calculatePriceForCR(
+          collateral,
+          tokensOutstanding,
+          this.empClient.collateralRequirement
+        );
+
+        // Sample message:
+        // Risk alert: [Tracked wallet name] has fallen below [threshold]%.
+        // Current [name of identifier] value: [current identifier value].
+        const mrkdwn =
+          wallet.name +
+          " (" +
+          createEtherscanLinkMarkdown(wallet.address, this.networkId) +
+          ") collateralization ratio has dropped to " +
+          this.formatDecimalString(positionCR.muln(100)) + // Scale up the CR threshold by 100 to become a percentage
+          "% which is below the " +
+          wallet.crAlert * 100 +
+          "% threshold. Current value of " +
+          this.syntheticCurrencySymbol +
+          " is " +
+          this.formatDecimalString(price) +
+          ". The collateralization requirement is " +
+          this.formatDecimalString(this.empClient.collateralRequirement.muln(100)) +
+          "%. If the price increases to " +
+          this.formatDecimalString(liquidationPrice) +
+          ", the position can be liquidated.";
+
+        this.logger.warn({
+          at: "ContractMonitor",
+          message: "Collateralization ratio alert 🙅‍♂️!",
+          mrkdwn: mrkdwn
+        });
+      }
+    }
+  }
+
+  _getPositionInformation(address) {
+    return this.empClient.getAllPositions().find(position => position.sponsor == address);
+  }
+
+  // Checks if a big number value is below a given threshold.
+  _ltThreshold(value, threshold) {
+    const { toBN, toWei } = this.web3.utils;
+    // If the price has not resolved yet then return false.
+    if (value == null) {
+      return false;
+    }
+    return toBN(value).lt(toBN(threshold));
+  }
+
+  // Calculate the collateralization Ratio from the collateral, token amount and token price
+  // This is cr = collateral / (tokensOutstanding * price)
+  _calculatePositionCRPercent(collateral, tokensOutstanding, tokenPrice) {
+    const { toBN, toWei } = this.web3.utils;
+    if (collateral == 0) {
+      return 0;
+    }
+    if (tokensOutstanding == 0) {
+      return null;
+    }
+    return toBN(collateral)
+      .mul(toBN(toWei("1")))
+      .mul(toBN(toWei("1")))
+      .div(toBN(tokensOutstanding).mul(toBN(tokenPrice)));
+  }
+
+  _calculatePriceForCR(collateral, tokensOutstanding, positionCR) {
+    const { toBN, toWei } = this.web3.utils;
+    const fixedPointScaling = toBN(toWei("1"));
+    return toBN(collateral)
+      .mul(fixedPointScaling)
+      .mul(fixedPointScaling)
+      .div(toBN(tokensOutstanding))
+      .div(toBN(positionCR));
+  }
+}
+
+module.exports = {
+  CRMonitor
+};
