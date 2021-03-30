723b50c02ab5dcfd2dc410adf23a60b34bd3be91
Socket1 <70174021+Socket1@users.noreply.github.com>
2021-03-30 13:47:40 -0400

    Initialize nick-commit

---
+++ disputer/disputer.js
@@ -0,0 +1,253 @@
+// When running this script it assumed that the account has enough tokens and allowance from the unlocked truffle
+// wallet to run the liquidations. Future versions will deal with generating additional synthetic tokens from EMPs as the bot needs.
+
+const { createObjectFromDefaultProps } = require("../common/ObjectUtils");
+
+class Disputer {
+  /**
+   * @notice Constructs new Disputer bot.
+   * @param {Object} logger Module used to send logs.
+   * @param {Object} expiringMultiPartyClient Module used to query EMP information on-chain.
+   * @param {Object} gasEstimator Module used to estimate optimal gas price with which to send txns.
+   * @param {Object} priceFeed Module used to get the current or historical token price.
+   * @param {String} account Ethereum account from which to send txns.
+   * @param {Object} [config] Contains fields with which constructor will attempt to override defaults.
+   */
+  constructor(logger, expiringMultiPartyClient, gasEstimator, priceFeed, account, config) {
+    this.logger = logger;
+    this.account = account;
+
+    // Expiring multiparty contract to read contract state
+    this.empClient = expiringMultiPartyClient;
+    this.web3 = this.empClient.web3;
+
+    // Gas Estimator to calculate the current Fast gas rate
+    this.gasEstimator = gasEstimator;
+
+    // Price feed to compute the token price.
+    this.priceFeed = priceFeed;
+
+    // Instance of the expiring multiparty to perform on-chain disputes
+    this.empContract = this.empClient.emp;
+
+    // Default config settings. Disputer deployer can override these settings by passing in new
+    // values via the `config` input object. The `isValid` property is a function that should be called
+    // before resetting any config settings. `isValid` must return a Boolean.
+    const defaultConfig = {
+      disputeDelay: {
+        // `disputeDelay`: Amount of time to wait after the request timestamp of the liquidation to be disputed.
+        // This makes the reading of the historical price more reliable. Denominated in seconds.
+        value: 60,
+        isValid: x => {
+          return x >= 0;
+        }
+      },
+      txnGasLimit: {
+        // `txnGasLimit`: Gas limit to set for sending on-chain transactions.
+        value: 9000000, // Can see recent averages here: https://etherscan.io/chart/gaslimit
+        isValid: x => {
+          return x >= 6000000 && x < 15000000;
+        }
+      }
+    };
+
+    // Validate and set config settings to class state.
+    Object.assign(this, createObjectFromDefaultProps(config, defaultConfig));
+  }
+
+  // Update the client and gasEstimator clients.
+  // If a client has recently updated then it will do nothing.
+  update = async () => {
+    await this.empClient.update();
+    await this.gasEstimator.update();
+    await this.priceFeed.update();
+  };
+
+  // Queries disputable liquidations and disputes any that were incorrectly liquidated.
+  queryAndDispute = async () => {
+    this.logger.debug({
+      at: "Disputer",
+      message: "Checking for any disputable liquidations"
+    });
+
+    await this.update();
+
+    // Get the latest disputable liquidations from the client.
+    const undisputedLiquidations = this.empClient.getUndisputedLiquidations();
+    const disputeableLiquidations = undisputedLiquidations.filter(
+      liquidation =>
+        this.empClient.isDisputable(
+          liquidation,
+          this.priceFeed.getHistoricalPrice(parseInt(liquidation.liquidationTime.toString()))
+        ) && this.empClient.getLastUpdateTime() >= Number(liquidation.liquidationTime) + this.disputeDelay
+    );
+
+    if (disputeableLiquidations.length === 0) {
+      this.logger.debug({
+        at: "Disputer",
+        message: "No disputable liquidations"
+      });
+      return;
+    }
+
+    for (const disputeableLiquidation of disputeableLiquidations) {
+      // Create the transaction.
+      const dispute = this.empContract.methods.dispute(disputeableLiquidation.id, disputeableLiquidation.sponsor);
+
+      // Simple version of inventory management: simulate the transaction and assume that if it fails, the caller didn't have enough collateral.
+      try {
+        await dispute.call({ from: this.account, gasPrice: this.gasEstimator.getCurrentFastPrice() });
+      } catch (error) {
+        this.logger.error({
+          at: "Disputer",
+          message: "Cannot dispute liquidation: not enough collateral (or large enough approval) to initiate dispute✋",
+          sponsor: disputeableLiquidation.sponsor,
+          liquidation: disputeableLiquidation,
+          error: error.toString()
+        });
+        continue;
+      }
+
+      const txnConfig = {
+        from: this.account,
+        gas: this.txnGasLimit,
+        gasPrice: this.gasEstimator.getCurrentFastPrice()
+      };
+
+      const disputeTime = parseInt(disputeableLiquidation.liquidationTime.toString());
+      const inputPrice = this.priceFeed.getHistoricalPrice(disputeTime).toString();
+
+      this.logger.debug({
+        at: "Disputer",
+        message: "Disputing liquidation",
+        liquidation: disputeableLiquidation,
+        inputPrice,
+        txnConfig
+      });
+
+      // Send the transaction or report failure.
+      let receipt;
+      try {
+        receipt = await dispute.send(txnConfig);
+      } catch (error) {
+        this.logger.error({
+          at: "Disputer",
+          message: "Failed to dispute liquidation🚨",
+          error: error.toString()
+        });
+        continue;
+      }
+
+      const logResult = {
+        tx: receipt.transactionHash,
+        sponsor: receipt.events.LiquidationDisputed.returnValues.sponsor,
+        liquidator: receipt.events.LiquidationDisputed.returnValues.liquidator,
+        id: receipt.events.LiquidationDisputed.returnValues.disputeId,
+        disputeBondPaid: receipt.events.LiquidationDisputed.returnValues.disputeBondAmount
+      };
+      this.logger.info({
+        at: "Disputer",
+        message: "Position has been disputed!👮‍♂️",
+        liquidation: disputeableLiquidation,
+        inputPrice,
+        txnConfig,
+        disputeResult: logResult
+      });
+    }
+
+    // Update the EMP Client since we disputed liquidations.
+    await this.empClient.update();
+  };
+
+  // Queries ongoing disputes and attempts to withdraw any pending rewards from them.
+  queryAndWithdrawRewards = async () => {
+    const { fromWei } = this.web3.utils;
+
+    this.logger.debug({
+      at: "Disputer",
+      message: "Checking for disputed liquidations that may have resolved"
+    });
+
+    await this.update();
+
+    // Can only derive rewards from disputed liquidations that this account disputed.
+    const disputedLiquidations = this.empClient
+      .getDisputedLiquidations()
+      .filter(liquidation => liquidation.disputer === this.account);
+
+    if (disputedLiquidations.length === 0) {
+      this.logger.debug({
+        at: "Disputer",
+        message: "No withdrawable disputes"
+      });
+      return;
+    }
+
+    for (const liquidation of disputedLiquidations) {
+      // Construct transaction.
+      const withdraw = this.empContract.methods.withdrawLiquidation(liquidation.id, liquidation.sponsor);
+
+      // Confirm that dispute has eligible rewards to be withdrawn.
+      let withdrawAmount;
+      try {
+        withdrawAmount = await withdraw.call({ from: this.account });
+      } catch (error) {
+        this.logger.debug({
+          at: "Disputer",
+          message: "No rewards to withdraw.",
+          liquidation: liquidation,
+          error: error.toString()
+        });
+        continue;
+      }
+
+      const txnConfig = {
+        from: this.account,
+        gas: this.txnGasLimit,
+        gasPrice: this.gasEstimator.getCurrentFastPrice()
+      };
+      this.logger.debug({
+        at: "Liquidator",
+        message: "Withdrawing dispute",
+        liquidation: liquidation,
+        amount: fromWei(withdrawAmount.rawValue),
+        txnConfig
+      });
+
+      // Send the transaction or report failure.
+      let receipt;
+      try {
+        receipt = await withdraw.send(txnConfig);
+      } catch (error) {
+        this.logger.error({
+          at: "Disputer",
+          message: "Failed to withdraw dispute rewards🚨",
+          error: error.toString()
+        });
+        continue;
+      }
+
+      const logResult = {
+        tx: receipt.transactionHash,
+        caller: receipt.events.LiquidationWithdrawn.returnValues.caller,
+        withdrawalAmount: receipt.events.LiquidationWithdrawn.returnValues.withdrawalAmount,
+        liquidationStatus: receipt.events.LiquidationWithdrawn.returnValues.liquidationStatus
+      };
+      this.logger.info({
+        at: "Disputer",
+        message: "Dispute withdrawn🤑",
+        liquidation: liquidation,
+        amount: fromWei(withdrawAmount.rawValue),
+        txnConfig,
+        liquidationResult: logResult
+      });
+    }
+
+    // Update the EMP Client since we withdrew rewards.
+    await this.empClient.update();
+  };
+}
+
+module.exports = {
+  Disputer
+};
---
+++ monitors/index.js
@@ -0,0 +1,207 @@
+require("dotenv").config();
+const { toWei } = web3.utils;
+
+// Helpers.
+const { delay } = require("../financial-templates-lib/helpers/delay");
+const { Logger } = require("../financial-templates-lib/logger/Logger");
+const { createPriceFeed } = require("../financial-templates-lib/price-feed/CreatePriceFeed");
+const { Networker } = require("../financial-templates-lib/price-feed/Networker");
+
+// Clients to retrieve on-chain data.
+const { ExpiringMultiPartyClient } = require("../financial-templates-lib/clients/ExpiringMultiPartyClient");
+const { ExpiringMultiPartyEventClient } = require("../financial-templates-lib/clients/ExpiringMultiPartyEventClient");
+const { TokenBalanceClient } = require("../financial-templates-lib/clients/TokenBalanceClient");
+
+// Monitor modules to report on client state changes.
+const { ContractMonitor } = require("./ContractMonitor");
+const { BalanceMonitor } = require("./BalanceMonitor");
+const { CRMonitor } = require("./CRMonitor");
+const { SyntheticPegMonitor } = require("./SyntheticPegMonitor");
+
+// Truffle contracts
+const ExpiringMultiParty = artifacts.require("ExpiringMultiParty");
+const ExpandedERC20 = artifacts.require("ExpandedERC20");
+
+// TODO: Figure out a good way to run this script, maybe with a wrapper shell script.
+// Currently, you can run it with `truffle exec ../monitor/index.js --price=<price>` *from the core  directory*.
+
+/**
+ * @notice Continuously attempts to monitor contract positions listening for newly emmited events.
+ * @param {Number} price Price used to inform the collateralization ratio of positions.
+ * @param {String} address Contract address of the EMP.
+ * @return None or throws an Error.
+ */
+async function run(
+  address,
+  shouldPoll,
+  botMonitorObject,
+  walletMonitorObject,
+  syntheticPegMonitorObject,
+  pollingDelay,
+  uniswapPriceFeedConfig,
+  medianizerPriceFeedConfig
+) {
+  try {
+    Logger.info({
+      at: "Monitor#index",
+      message: "Monitor started 🕵️‍♂️",
+      empAddress: address,
+      pollingDelay: pollingDelay,
+      botMonitorObject,
+      walletMonitorObject,
+      uniswapPriceFeedConfig,
+      medianizerPriceFeedConfig
+    });
+
+    // Setup web3 accounts an contract instance
+    const accounts = await web3.eth.getAccounts();
+    const emp = await ExpiringMultiParty.at(address);
+
+    // Setup price feed.
+    // TODO: consider making getTime async and using contract time.
+    const getTime = () => Math.round(new Date().getTime() / 1000);
+    const medianizerPriceFeed = await createPriceFeed(web3, Logger, new Networker(Logger), getTime, priceFeedConfig);
+
+    if (!uniswapPriceFeed || !medianizerPriceFeed) {
+      throw "Invalid price feed config";
+    }
+
+    // 1. Contract state monitor
+    const empEventClient = new ExpiringMultiPartyEventClient(Logger, ExpiringMultiParty.abi, web3, emp.address, 10);
+    const contractMonitor = new ContractMonitor(
+      Logger,
+      empEventClient,
+      [accounts[0]],
+      [accounts[0]],
+      medianizerPriceFeed
+    );
+
+    // 2. Balance monitor
+    const collateralTokenAddress = await emp.collateralCurrency();
+    const syntheticTokenAddress = await emp.tokenCurrency();
+
+    const tokenBalanceClient = new TokenBalanceClient(
+      Logger,
+      ExpandedERC20.abi,
+      web3,
+      collateralTokenAddress,
+      syntheticTokenAddress,
+      10
+    );
+
+    const balanceMonitor = new BalanceMonitor(Logger, tokenBalanceClient, botMonitorObject);
+
+    // 3. Collateralization Ratio monitor.
+    const empClient = new ExpiringMultiPartyClient(Logger, ExpiringMultiParty.abi, web3, emp.address, 10);
+
+    const crMonitor = new CRMonitor(Logger, empClient, walletMonitorObject, medianizerPriceFeed);
+
+    // 4. Synthetic Peg Monitor.
+    const uniswapPriceFeed = await createPriceFeed(web3, Logger, new Networker(Logger), getTime, priceFeedConfig);
+    const syntheticPegMonitor = new SyntheticPegMonitor(
+      Logger,
+      web3,
+      uniswapPriceFeed,
+      medianizerPriceFeed,
+      syntheticPegMonitorObject
+    );
+
+    while (true) {
+      // 1.  Contract monitor
+      // 1.a Update the client
+      await empEventClient.update();
+      await medianizerPriceFeed.update();
+      // 1.b Check For new liquidation events
+      await contractMonitor.checkForNewLiquidations();
+      // 1.c Check for new disputes
+      await contractMonitor.checkForNewDisputeEvents();
+      // 1.d Check for new disputeSettlements
+      await contractMonitor.checkForNewDisputeSettlementEvents();
+
+      // 2.  Wallet Balance monitor
+      // 2.a Update the client
+      await tokenBalanceClient.update();
+      // 2.b Check for monitored bot balance changes
+      await balanceMonitor.checkBotBalances();
+
+      // 3.  Position Collateralization Ratio monitor.
+      // 3.a Update the client
+      await empClient.update();
+      // 3.b Check for positions below their CR
+      await crMonitor.checkWalletCrRatio();
+
+      // 4. Synthetic peg monitor
+      // 4.a Update the price feeds
+      await uniswapPriceFeed.update();
+      await medianizerPriceFeed.update();
+      // 4.b Check for synthetic peg deviation
+      await syntheticPegMonitor.checkPriceDeviation();
+
+      await delay(Number(pollingDelay));
+
+      if (!shouldPoll) {
+        break;
+      }
+    }
+  } catch (error) {
+    Logger.error({
+      at: "Monitors#index",
+      message: "Monitor polling error. Monitor crashed🚨",
+      error: error.toString()
+    });
+    await delay(5000); // Hacky fix to ensure that winston still fires messages upstream.
+  }
+}
+const Poll = async function(callback) {
+  try {
+    if (!process.env.EMP_ADDRESS) {
+      throw "Bad environment variables! Specify an `EMP_ADDRESS` for the location of the expiring Multi Party.";
+    }
+
+    if (!process.env.BOT_MONITOR_OBJECT || !process.env.WALLET_MONITOR_OBJECT) {
+      throw "Bad input arg! Specify a `BOT_MONITOR_OBJECT` & `WALLET_MONITOR_OBJECT` to track.";
+    }
+
+    const pollingDelay = process.env.POLLING_DELAY ? process.env.POLLING_DELAY : 10000;
+
+    // Bot objects to monitor. For each bot specify a name, address and the thresholds to monitor.
+    const botMonitorObject = JSON.parse(process.env.BOT_MONITOR_OBJECT);
+
+    // Wallet objects to monitor.
+    const walletMonitorObject = JSON.parse(process.env.WALLET_MONITOR_OBJECT);
+
+    if (!process.env.UNISWAP_PRICE_FEED_CONFIG || !process.env.MEDIANIZER_PRICE_FEED_CONFIG) {
+      throw "Bad input arg! Specify `PRICE_FEED_CONFIG` and `MEDIANIZER_PRICE_FEED_CONFIG` to define the price feed settings.";
+    }
+
+    // Read price feed configuration from an environment variable.
+    const uniswapPriceFeedConfig = JSON.parse(process.env.UNISWAP_PRICE_FEED_CONFIG);
+    const medianizerPriceFeedConfig = JSON.parse(process.env.MEDIANIZER_PRICE_FEED_CONFIG);
+
+    if (!process.env.SYNTHETIC_PEG_MONITOR_OBJECT) {
+      throw "Bad input arg! Specify `SYNTHETIC_PEG_OBJECT` to parameterize the Synthetic peg monitor.";
+    }
+
+    // Read the synthetic peg monitor config from an environment variable.
+    const syntheticPegMonitorObject = JSON.parse(process.env.SYNTHETIC_PEG_MONITOR_OBJECT);
+
+    await run(
+      process.env.EMP_ADDRESS,
+      true,
+      botMonitorObject,
+      walletMonitorObject,
+      syntheticPegMonitorObject,
+      pollingDelay,
+      uniswapPriceFeedConfig,
+      medianizerPriceFeedConfig
+    );
+  } catch (err) {
+    callback(err);
+  }
+  callback();
+};
+
+// Attach this function to the exported function
+// in order to allow the script to be executed through both truffle and a test runner.
+Poll.run = run;
+module.exports = Poll;
