6758ad8d21e379a3bca4a4b576433399a122bf5e
Socket1 <70174021+Socket1@users.noreply.github.com>
2021-03-30 13:48:40 -0400

    Initialize chrismaree/dapp-node

---
+++ packages/core/migrations/11_support_identifiers.js
@@ -0,0 +1,14 @@
+const IdentiferWhitelist = artifacts.require("IdentifierWhitelist");
+const { getKeysForNetwork } = require("@uma/common");
+const identifiers = require("../config/identifiers");
+
+module.exports = async function(deployer, network, accounts) {
+  const keys = getKeysForNetwork(network, accounts);
+
+  const supportedIdentifiers = await IdentiferWhitelist.deployed();
+
+  for (const identifier of Object.keys(identifiers)) {
+    const identifierBytes = web3.utils.utf8ToHex(identifier);
+    await supportedIdentifiers.addSupportedIdentifier(identifierBytes, { from: keys.deployer });
+  }
+};
